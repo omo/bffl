7a5f16f4cec959e00249da4ec2badb607870d5d1
Hajime Morrita <omo@dodgson.org>
2014-10-28 21:58:14 -0700

    Extend Shuttles. Needs Test.

--- app/src/main/java/es/flakiness/shuttle/CentralStation.java
+++ app/src/main/java/es/flakiness/shuttle/CentralStation.java
@@ -1,6 +1,8 @@
 package es.flakiness.shuttle;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class CentralStation implements Station {
@@ -21,21 +23,35 @@ public class CentralStation implements Station {
         return instance;
     }
 
+    private String mName;
+    private Executor mExecutor;
     private HandlerShuttle mMain;
     private ExecutorShuttle mBackground;
+    private ConcurrentHashMap<Object, SequentialShuttle> mBoundSequences = new ConcurrentHashMap<Object, SequentialShuttle>();
 
-    public CentralStation(HandlerShuttle main, ExecutorShuttle background) {
-        mMain = main;
-        mBackground = background;
+    public CentralStation(String name, Executor executor) {
+        mName = name;
+        mExecutor = executor;
+        mMain = new HandlerShuttle(name + "Main");
+        mBackground = new ExecutorShuttle(name + "Background", executor);
     }
 
     public CentralStation(String name) {
-        mMain = new HandlerShuttle(name + "Main");
-        mBackground = new ExecutorShuttle(name + "Background", Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
+        this(name, Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
     }
 
     public CentralStation() {
         this("");
+    }
+
+    @Override
+    public SequentialShuttle sequential(Object key) {
+        SequentialShuttle found = mBoundSequences.get(key);
+        if (found != null)
+            return found;
+        SequentialShuttle shuttle = new SequentialShuttle(mName + key.toString(), mExecutor);
+        SequentialShuttle previous = mBoundSequences.putIfAbsent(key, shuttle);
+        return null != previous ? previous : shuttle;
     }
 
     @Override
---
+++ app/src/main/java/es/flakiness/shuttle/SequentialShuttle.java
@@ -0,0 +1,55 @@
+package es.flakiness.shuttle;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SequentialShuttle extends ShuttleBus {
+
+    private AtomicBoolean mRunning = new AtomicBoolean(false);
+    private ConcurrentLinkedQueue<Object> mQueue = new ConcurrentLinkedQueue();
+    private Executor mExecutor;
+
+    @Override
+    public void post(Object event) {
+        mQueue.offer(event);
+        activateIfNeeded();
+    }
+
+    private void activateIfNeeded() {
+        if (mQueue.isEmpty())
+            return;
+        if (!mRunning.compareAndSet(false, true))
+            return;
+        mExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                Object taken = mQueue.poll();
+                if (taken != null)
+                    postToBus(taken);
+                //
+                // Here is a tricky part. My reasoning:
+                //
+                // * It can become from empty to non-empty. In that case, the thread who
+                //   offer()-ed the item must have schedule the task.
+                //
+                // * It can become no-empty to empty. In that case, the activation is a waste
+                //   but it is OK as it doesn't hurt.
+                //
+                // * Is it possible for offere()-ed thread to miss its own activation?
+                //   I don't think so. mRunning is set to false before examining isEmpty()
+                //   So either this thread or offer()-ed thread should be able to activate
+                //   the shuttle.
+                //
+                // However TODO(morrita): Probably I should write a test for this somehow.
+                mRunning.set(false);
+                activateIfNeeded();
+            }
+        });
+    }
+
+    public SequentialShuttle(String name, Executor executor) {
+        super(name);
+        mExecutor = executor;
+    }
+}
